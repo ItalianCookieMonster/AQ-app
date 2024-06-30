b4798c873ee1ecd8b313b523bb0ad2a4003217b6
redondo <rdnd201@gmail.com>
2024-06-30 11:42:54 +0200

    task 3 connected

--- src/components/CustomQuestion/CustomQuestion.jsx
+++ src/components/CustomQuestion/CustomQuestion.jsx
@@ -1,32 +1,37 @@
 /* eslint-disable react/prop-types */
-import { Textarea } from "@/components/ui/textarea"
-import { Button } from "../ui/button"
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "../ui/button";
 import { useState } from "react";
 
-const CustomQuestion = ({ title }) => {
-    const [question, setQuestion] = useState("");
+const CustomQuestion = ({ title, handleSubmitQuestion }) => {
+  const [question, setQuestion] = useState("");
 
-    const handleQuestionChange = (event) => {
-        setQuestion(event.target.value);
-    };
+  const handleQuestionChange = (event) => {
+    setQuestion(event.target.value);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(question);
-
-    };
-    return (
-        <div className="border border-solid p-3 rounded-[20px] mt-8 w-full flex flex-col items-center justify-center gap-5">
-            <p className="text-lg font-bold">{title}</p>
-            <form onSubmit={handleSubmit} className="w-full flex flex-col items-center justify-center gap-5">
-                <Textarea
-                    className="w-[80%]"
-                    placeholder="Type your question here..."
-                    value={question}
-                    onChange={handleQuestionChange} />
-                <Button type="submit" className="block w-[80%]">Send message</Button>
-            </form>
-        </div>
-    )
-}
-export default CustomQuestion
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleSubmitQuestion(question);
+  };
+  return (
+    <div className="border border-solid p-3 rounded-[20px] mt-8 w-full flex flex-col items-center justify-center gap-5">
+      <p className="text-lg font-bold">{title}</p>
+      <form
+        onSubmit={handleSubmit}
+        className="w-full flex flex-col items-center justify-center gap-5"
+      >
+        <Textarea
+          className="w-[80%]"
+          placeholder="Type your question here..."
+          value={question}
+          onChange={handleQuestionChange}
+        />
+        <Button type="submit" className="block w-[80%]">
+          Send message
+        </Button>
+      </form>
+    </div>
+  );
+};
+export default CustomQuestion;
--- src/components/TypingText/TypingText.jsx
+++ src/components/TypingText/TypingText.jsx
@@ -1,33 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
+// eslint-disable-next-line react/prop-types
+const TypingEffect = ({ textFromOutput }) => {
+  const [text, setText] = useState(textFromOutput);
+  const [displayText, setDisplayText] = useState("");
 
+  useEffect(() => {
+    if (textFromOutput) {
+      setText(textFromOutput);
+    }
+  }, [textFromOutput]);
 
-const TypingEffect = () => {
-    const [text, setText] = useState('');
-    const [displayText, setDisplayText] = useState('');
-    let stringToTest = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxim mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis haru`;
+  useEffect(() => {
+    let index = 0;
+    const intervalId = setInterval(() => {
+      if (index < text.length) {
+        setDisplayText((prev) => prev + text.charAt(index));
+        index++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 50);
 
-    setText(stringToTest);
+    return () => clearInterval(intervalId);
+  }, [text]);
 
-    useEffect(() => {
-        let index = 0;
-        const intervalId = setInterval(() => {
-            if (index < text.length) {
-                setDisplayText((prev) => prev + text.charAt(index));
-                index++;
-            } else {
-                clearInterval(intervalId);
-            }
-        }, 50); 
-
-        return () => clearInterval(intervalId);
-    }, [text]);
-
-    return (
-        <div className="typing-effect">
-            {stringToTest}
-        </div>
-    );
+  return <div className="typing-effect">{displayText}</div>;
 };
 
 export default TypingEffect;

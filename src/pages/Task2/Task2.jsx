d1ec5facdc135913c0b43981f364f751191175f8
redondo <rdnd201@gmail.com>
2024-06-30 11:11:08 +0200

    task 2 connected

--- src/pages/Task2/Task2.jsx
+++ src/pages/Task2/Task2.jsx
@@ -1,59 +1,80 @@
+import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getAirQualityInfo } from "../../api/getAirQualityInfo";
+import { enhanceAirOutput } from "../../utils/tansformOutput";
 import Loading from "@/components/Loading/Loading";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
 import SpeedoMeter from "@/components/SpeedoMeter/SpeedoMeter";
 
-
-
-
-
-
 const Task2 = () => {
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState({});
+  const [output, setOutput] = useState("");
+  const [currentAirStatus, setCurrentAirStatus] = useState(undefined);
 
-    const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const currentUserData = localStorage.getItem("user-data");
+    setUserData(currentUserData ? JSON.parse(currentUserData) : {});
+  }, []);
 
+  const {
+    data: airQualityInfoData,
+    // error: airQualityInfoError,
+    // isFetching: isFetchingairQualityInfo,
+    // refetch: refetchairQualityInfo,
+  } = useQuery({
+    queryKey: ["airQualityInfo", userData],
+    queryFn: async () => {
+      if (userData) {
+        // const { city } = userData;
 
+        const result = await getAirQualityInfo({
+          city: "Barcelona", // TODO: pending get city!!!!!
+          userParams: userData,
+          question: "currentAirSituation",
+        });
+        return result;
+      }
+    },
+    enabled: !!userData,
+    retry: 2,
+  });
 
+  useEffect(() => {
+    if (airQualityInfoData && airQualityInfoData.questionOutput) {
+      const { currentAirSituation, currentAirStatus } =
+        airQualityInfoData.questionOutput;
 
-    return (
-        <>
+      const enhancedOutput = enhanceAirOutput(currentAirSituation);
 
-            {loading ?
-                <Loading />
-                :
-                <div className="flex flex-col gap-7 lg:max-w-[70vw] justify-center" >
-                    <h1 className="text-2xl font-bold text-center mb-5 text-primary">Message</h1>
+      setOutput(enhancedOutput);
+      setCurrentAirStatus(currentAirStatus);
 
-                    <div className="flex-col md:flex-row justify-center">
-                        <div className="flex justify-center items-center w-full">
-                            <SpeedoMeter />
-                        </div>
+      setLoading(false);
+    }
+  }, [airQualityInfoData]);
 
-                        <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-                            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-                            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-                            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-                            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-                            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-                            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-                            recusandae alias error harum maxime adipisci amet laborum
-                        </p>
+  return loading ? (
+    <Loading />
+  ) : (
+    <div className="flex flex-col gap-7 lg:max-w-[70vw] justify-center">
+      <h1 className="text-2xl font-bold text-center mb-5 text-primary">
+        Message
+      </h1>
 
-                    </div>
+      <div className="flex-col md:flex-row justify-center">
+        <div className="flex justify-center items-center w-full">
+          <SpeedoMeter currentAirStatus={currentAirStatus} />
+        </div>
 
-                    <Link to={'/home'} className="flex items-center justify-center">
-                        <Button className="w-[50vw] lg:w-[30vw] mt-8">
-                            Go Back
-                        </Button>
-                    </Link>
-                </div>
-            }
+        <p className="text-center">{output}</p>
+      </div>
 
-        </>
-
-    )
-}
-export default Task2
+      <Link to={"/home"} className="flex items-center justify-center">
+        <Button className="w-[50vw] lg:w-[30vw] mt-8">Go Back</Button>
+      </Link>
+    </div>
+  );
+};
+export default Task2;

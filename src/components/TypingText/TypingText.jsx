3b8432e5f8c57f1e8cce8b9708a47eeef606d731
Valentina Toni <vale.toni93@gmail.com>
2024-06-30 10:25:50 +0200

    add done task 3 styling

--- src/components/TypingText/TypingText.jsx
+++ src/components/TypingText/TypingText.jsx
@@ -1,19 +1,15 @@
 import { useState, useEffect } from 'react';
-
-
+import './TypingText.css';
 
 const TypingEffect = () => {
-    const [text, setText] = useState('');
     const [displayText, setDisplayText] = useState('');
-    let stringToTest = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxim mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis haru`;
-
-    setText(stringToTest);
+    const stringToTest = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxim mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum`;
 
     useEffect(() => {
         let index = 0;
         const intervalId = setInterval(() => {
-            if (index < text.length) {
-                setDisplayText((prev) => prev + text.charAt(index));
+            if (index < stringToTest.length) {
+                setDisplayText(prev => prev + stringToTest.charAt(index));
                 index++;
             } else {
                 clearInterval(intervalId);
@@ -21,13 +17,14 @@ const TypingEffect = () => {
         }, 50); 
 
         return () => clearInterval(intervalId);
-    }, [text]);
+    }, []); 
 
     return (
         <div className="typing-effect">
-            {stringToTest}
+            {displayText}
         </div>
     );
 };
 
 export default TypingEffect;
+
--- src/pages/Task3/Task3.jsx
+++ src/pages/Task3/Task3.jsx
@@ -1,24 +1,20 @@
 import CustomQuestion from "@/components/CustomQuestion/CustomQuestion"
 import { Link } from "react-router-dom"
-import { Button } from "@/components/ui/button"
 import TypingEffect from "@/components/TypingText/TypingText"
+import { MoveLeft } from "lucide-react"
 
 const Task3 = () => {
     return (
-        <div className="w-[80vw] flex flex-col items-center">
-            <CustomQuestion title="What are you doing right now?" />
-
-            <div className="min-h-[300px]">
-                    <TypingEffect/>
+        <div className="w-[80vw] flex flex-col items-center gap-5 text-center">
+            <h2 className="w-full text-lg font-medium lg:w-[60vw] max-w-[800px]">{`Hey! Just checking in to see what’s happening on your end. Your current activities could give us great clues on how to boost your air quality. Mind sharing what you're doingy`}</h2>
+            <div className="min-h-[200px] flex justify-center p-5">
+                <TypingEffect />
 
             </div>
+            <CustomQuestion />
 
-
-
-            <Link to={'/home'} className="justify-self-end">
-                <Button className="w-[50vw] mt-8">
-                    Go Back
-                </Button>
+            <Link to={'/home'} className=" border border-solid border-transparent justify-self-end hover:text-primary hover:border-primary rounded-[50%] p-4">
+                <MoveLeft />
             </Link>
         </div>
 
